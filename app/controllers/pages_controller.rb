d312703fe71b4a18504bea5a4004b633b047a33f
Pierre Liduena <assurancesvelo@gmail.com>
2016-11-12 23:07:40 +0100

    add moel produit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
+
   def home
+    @today = Date.today
   end
+
+
+  def team
+    @members = ["bob", "romain", "seb"]
+  end
+
+
+  def join_us
+
+  end
+
 end
